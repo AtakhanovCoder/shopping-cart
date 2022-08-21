5419f9b42e8fc34cf7b056d341aa5671f31645e6
Toyibkhan <atakhanov2007@gmail.com>
2022-08-21 14:58:02 +0500

    header section finished

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,55 @@
 <template>
-  
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/shop">Shop</router-link>
+
+    <router-link to="/about">About</router-link>
+    <router-link to="/contact">Contact</router-link>
+    <router-link to="/cart"><i class="fa-solid fa-bag-shopping"></i><span class="notif">0</span></router-link>
+  </nav>
+  <router-view/>  
 </template>
 <style>
-/* nav a.router-link-exact-active {
-} */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-decoration: none;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+  color: #293462;
+}
+:root{
+  --purple: #7F31FF;
+  --black-gray: #1D1D1C;
+}
+nav a{
+  font-size: 36px;
+  color: white;
+}
+nav{
+  display: flex;
+  justify-content: space-evenly;
+  border-bottom: 5px solid var(--purple);
+  padding: 10px;
+  background-color: var(--black-gray);
+}
+nav a.router-link-exact-active, nav a.router-link-exact-active i {
+  color: var(--purple);
+}
+nav a i{
+  color: white;
+}
+.notif{
+  position: relative;
+  bottom: 20px;
+  right: 10px;
+  font-size: 20px;
+  background-color: red;
+  color: #fff;
+  padding: 0px 7px;
+  border-radius: 50px;
+}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Contact from '../views/Contact.vue'
+import Cart from '../views/Cart.vue'
+import Shop from '../views/Shop.vue'
 const routes = [
-  
+{
+  path: '/',
+  name: 'home',
+  component: Home,
+},  
+{
+  path: '/shop',
+  name: 'shop',
+  component: Shop,
+},  
+{
+  path: '/about',
+  name: 'about',
+  component: About,
+},
+{
+  path: '/contact',
+  name: 'contact',
+  component: Contact,
+},
+{
+  path: '/cart',
+  name: 'cart',
+  component: Cart,
+},
 ]
 
 const router = createRouter({
