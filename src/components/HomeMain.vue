dff617581be7dd6ea4a714a01775d335e8f55a61
Toyibkhan <atakhanov2007@gmail.com>
2022-08-21 21:18:01 +0500

    home page main section finished

---
+++ src/components/HomeMain.vue
@@ -0,0 +1,94 @@
+<template>
+  <main>
+    <div class="main-title">
+      <!-- <h1>we have very comfortable <br>
+      stylish and unique clothes</h1>
+      <h2>ULTRABOOST CLIMACOOL 1 DNA Sneaker</h2>
+      <span>172$</span>
+      <span>155$</span> -->
+      <h3>
+      stylish and unique clothes
+      </h3>
+      <h1>ULTRABOOST CLIMA <br>COOL1 DNA Sneaker</h1>
+      <button class="price-1">150$</button>
+      <button class="price-2">100$</button>
+    </div>
+    <div class="main-photo">
+      <img src="../assets/Sneakers/sneaker-adidas-1.jpg" alt="">
+    </div>
+  </main>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+:root{
+  --purple: #7F31FF;
+  --black-gray: #1D1D1C;
+}
+main{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  min-height: 90vh;
+}
+.main-title h1{
+  color: var(--purple);
+  padding: 10px;
+}
+.main-title h3{
+  margin-left: 0px;
+  font-size: 20px;
+  padding: 10px;
+}
+.main-title .price-1{
+  margin-top: 10px;
+  margin-left: 50px;
+  width: 100px;
+  height: 45px;
+  font-size: 29px;
+  border: none;
+  background-color: var(--purple);
+  color: white;
+  font-weight: bold;
+  text-decoration: line-through;
+}
+.main-title .price-2 {
+  margin-left: 20px;
+  width: 100px;
+  height: 45px;
+  font-size: 29px;
+  border: none;
+  background-color: var(--purple);
+  color: white;
+  font-weight: bold;
+}
+.main-photo img{
+  width: 100%;
+  height: 60vh;
+  border-radius: 50%;
+  border: 10px solid var(--purple);
+}
+@media (max-width: 450px){
+  main{
+    display: flex;
+    flex-wrap: wrap;
+  }
+}
+@media (max-width: 750px) {
+  main{
+    display: flex;
+    flex-direction: column;
+    gap: 50px;
+  }
+  .main-photo img{
+  width: 100%;
+  height: 40vh;
+  }
+  .main-title h1, .main-title h3{
+    text-align: center;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,12 @@
 <template>
   <main>
-
+    <HomeMain/>
   </main>
 </template>
 <script>
+import HomeMain from '@/components/HomeMain.vue';
 export default {
-  
+    components: { HomeMain }
 }
 </script>
 <style>
